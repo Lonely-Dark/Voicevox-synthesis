0aa07cd3daf8d96d22df2d1adc5b85f83c1a09a1
Vlad Korepov <vlad10game@gmail.com>
2023-03-24 21:26:37 +0300

    New version v0.1.1
    Now on full asyncio

--- src/main.py
+++ src/main.py
@@ -10,38 +10,60 @@ import logging
 
 import simpleaudio as sa
 import speech_recognition as sr
+from aiofile import async_open
 from helperclass_pack import Helper
 from translatepy import Translator
+from translatepy.models import TranslationResult
 from voicevox import Client
 
 
-async def main(voice_id: int = 1):
-    r = sr.Recognizer()
-    tr = Translator()
-    log = Helper()
-    log.logger.setLevel(logging.DEBUG)
+class Recogniser:
+    def __init__(self) -> None:
+        self.r = sr.Recognizer()
+        self.tr = Translator()
+        self.log = Helper().logger
+        self.log.setLevel(logging.DEBUG)
 
-    log.logger.info("Starting...")
-    with sr.Microphone() as source:
-        while True:
-            log.logger.info("Listening...")
-            audio = r.listen(source)
+        self.log.info("Initializing recogniser...")
 
-            try:
-                text = r.recognize_google(audio)
-            except sr.UnknownValueError:
-                log.logger.critical('Google Speech Recognition could not understand audio')
-                continue
-            log.logger.debug("You said: {}".format(text))
-            translated = tr.translate(text, 'Japanese')
-            log.logger.debug("Translated: {}".format(translated))
+    async def listenMicro(self) -> sr.AudioData:
+        with sr.Microphone() as source:
+            self.log.info("Listening...")
+            audio = self.r.listen(source)
+        return audio
 
-            async with Client() as client:
-                audio_query = await client.create_audio_query(translated, speaker=voice_id)
-                with open('voice.wav', 'wb') as f:
-                    f.write(await audio_query.synthesis())
+    async def recognise(self, audio: sr.AudioData) -> str:
+        try:
+            text = self.r.recognize_google(audio, language='ru-RU')
+            self.log.debug(f"Recognized text: {text}")
+            return text
+        except sr.UnknownValueError:
+            self.log.error('Google Speech Recognition could not understand audio')
 
-            sa.WaveObject.from_wave_file('voice.wav').play()
+    async def translate(self, text: str) -> TranslationResult:
+        translated = self.tr.translate(text, 'Japanese')
+        self.log.debug(f"Translated text: {translated}")
+        return translated
+
+    async def synthesize(self, text: TranslationResult, voice_id: int = 1, filename: str = 'voice.wav') -> None:
+        async with Client() as client:
+            audio_query = await client.create_audio_query(text, speaker=voice_id)
+            async with async_open(filename, 'wb') as f:
+                await f.write(await audio_query.synthesis())
+            self.log.info(f"Synthesized {filename}")
+
+    async def play(self, filename: str) -> None:
+        self.log.debug(f"Playing {filename}")
+        sa.WaveObject.from_wave_file(filename).play()
+
+
+async def main() -> None:
+    recogniser: Recogniser = Recogniser()
+    audio: sr.AudioData = await recogniser.listenMicro()
+    text: str = await recogniser.recognise(audio)
+    translated: TranslationResult = await recogniser.translate(text)
+    await recogniser.synthesize(translated, voice_id=1)
+    await recogniser.play('voice.wav')
 
 
 if __name__ == '__main__':
